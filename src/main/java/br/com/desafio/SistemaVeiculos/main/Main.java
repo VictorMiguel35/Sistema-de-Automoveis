756f063aa0924c6884bfbbdfc8cc188799e86f5c
Victor Miguel <victorgabrielam12@gmail.com>
2025-09-26 20:33:18 -0300

    Adicionando filtro pelo nome do veiculo e consulta de valores

--- src/main/java/br/com/desafio/SistemaVeiculos/main/Main.java
+++ src/main/java/br/com/desafio/SistemaVeiculos/main/Main.java
@@ -1,11 +1,14 @@
 package br.com.desafio.SistemaVeiculos.main;
 
 import br.com.desafio.SistemaVeiculos.model.Dados;
+import br.com.desafio.SistemaVeiculos.model.DadosVeiculo;
 import br.com.desafio.SistemaVeiculos.model.Modelos;
 import br.com.desafio.SistemaVeiculos.service.ConsumoApi;
 import br.com.desafio.SistemaVeiculos.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,15 +21,15 @@ public class Main {
 
     public void exibeMenu() {
         System.out.println("""
-                Escolha o tipo de veículo:
-                Carros
-                Caminhoes
-                Motos
+                Digite o tipo de veículo:
+                Carro
+                Caminhão
+                Moto
                 """);
-        var veiculo = scanner.nextLine().toLowerCase();
-        if (veiculo.toLowerCase().contains("carr")) {
+        var opcao = scanner.nextLine().toLowerCase();
+        if (opcao.toLowerCase().contains("car")) {
             endereco = url + "carros/marcas";
-        } else if (veiculo.toLowerCase().contains("mot")) {
+        } else if (opcao.toLowerCase().contains("mot")) {
             endereco = url + "motos/marcas";
         } else {
             endereco = url + "caminhoes/marcas";
@@ -37,16 +40,38 @@ public class Main {
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
 
-        System.out.println("Informe o código da marca para consulta: ");
-        var codigoMarca = scanner.nextLine();
+        System.out.println("\nInforme o código da marca para consulta: ");
+        var codigoMarca = scanner.next() + scanner.nextLine();
 
         endereco = endereco + "/" + codigoMarca + "/modelos";
         json = consumoApi.obeterDados(endereco);
         var modeloLista = converteDados.obterDados(json, Modelos.class);
+        System.out.println("\nModelos dessa marca: \n");
         modeloLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
 
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado: ");
+        var nomeVeiculo = scanner.nextLine();
+        modeloLista.modelos().stream()
+                .filter(d -> d.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("\nDigite o código do modelo para consultar valores: ");
+        var codigoModelo = scanner.nextLine();
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = consumoApi.obeterDados(endereco);
+        var anos = converteDados.obterLista(json, Dados.class);
+
+        List<DadosVeiculo> veiculos = new ArrayList<>();
+
+        for (int i=0; i<anos.size(); i++) {
+            json = consumoApi.obeterDados(endereco + "/" + anos.get(i).codigo());
+            var resultado = converteDados.obterDados(json, DadosVeiculo.class);
+            veiculos.add(resultado);
+        }
+        veiculos.forEach(System.out::println);
     }
 
 }
